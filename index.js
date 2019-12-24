ca16bbbab84e02354de3432a4f1bf6a20334f770
unibreakfast <unibreakfast1@gmail.com>
2019-12-24 08:58:44 +0200

    .updUser method implemented, tests passed

--- index.js
+++ index.js
@@ -8,16 +8,24 @@ jsonStore = require('./jsonstore'),
 addArr = async (...names)=>
   await update(store => names.forEach(name => store[name] = store[name] || [])),
 
+select =(store, opt)=> store.users.find(
+  typeof opt=='object'? u => entries(opt).every(([key, val])=> u[key]==val) :
+    typeof opt=='number'? u => u.id==opt : 0) || null
+
+user = async opt => await read(store => select(store, opt)),
+
 addUser = async (login, hash)=>
   await update((store, newId)=> store.users.find(u => u.login==login)? false :
     store.users.push({id: newId=newId(), login, hash}) && newId),
 
-user = async opt => await read(store => store.users.find(
-  typeof opt=='object'? u => entries(opt).every(([key, val])=> u[key]==val) :
-    typeof opt=='number'? u => u.id==opt : 0) || null),
+updUser = async (opt, upd)=> await update(store => {
+  const user = select(store, opt)
+  return user? assign(user, upd) && true : false
+}),
 
 link = async str => assign(module.exports,
-  {db: { read, update } = db = await jsonStore(str), addArr, addUser, user})
+  {db: { read, update } = db = await jsonStore(str),
+    addArr, user, addUser, updUser})
 
 
 module.exports = {link}
