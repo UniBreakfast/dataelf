45dcd36b442046a619c2a7696b8fb330dce9df46
unibreakfast <unibreakfast1@gmail.com>
2019-12-25 08:02:19 +0200

    select tweak

--- index.js
+++ index.js
@@ -8,18 +8,18 @@ jsonStore = require('./jsonstore'),
 addArr = async (...names)=>
   await update(store => names.forEach(name => store[name] = store[name] || [])),
 
-select =(store, opt)=> store.users.find(
+select =(arr, opt)=> arr.find(
   typeof opt=='object'? u => entries(opt).every(([key, val])=> u[key]==val) :
     typeof opt=='number'? u => u.id==opt : 0) || null
 
-user = async opt => await read(store => select(store, opt)),
+user = async opt => await read(store => select(store.users, opt)),
 
 addUser = async (login, hash)=>
   await update((store, newId)=> store.users.find(u => u.login==login)? false :
     store.users.push({id: newId=newId(), login, hash}) && newId),
 
 updUser = async (opt, upd)=> await update(store => {
-  const user = select(store, opt)
+  const user = select(store.users, opt)
   return user? assign(user, upd) && true : false
 }),
 
