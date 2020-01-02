2b36e53d6796e80b0cb133f1db97b11f2d372403
unibreakfast <unibreakfast1@gmail.com>
2020-01-02 21:47:18 +0200

    addUser method tested with mongo

--- dataelf.test.js
+++ dataelf.test.js
@@ -6,27 +6,24 @@ const
   csv =(...strs)=> [].concat(...strs.map(str => str.split(', '))),
 
   dataElf = require('.'),
+  { MongoClient, ObjectId } = require('mongodb'),
 
+  mongoOptions = {useUnifiedTopology: true},
   mongoConnect = 'mongodb+srv://testman:k9OgRrtXvly4U2vm'+
     '@gfcluster-o2r1m.mongodb.net/test?retryWrites=true&w=majority',
+  dbName = 'testdb',
+
+  methods = csv('user, addUser, updUser',
+    'ses, addSes, updSes, delSes, sess, siftSess'),
+  absent = [],
 
   runTests = async ()=> {
     await link()
+    await addUser(1)
   },
 
-  // Expectations from dataElf module are as follows:
-  // exports an object
-  // it has a link method
-    // it takes a connection string and options
-    // it returns the same object
-    // as side effect it adds to that object a db reference as a prop
-    // and multiple methods to work through it
-
-  link = makeTest("dataElf module supposed to be exporting an object with .link method to initialize the dataElf with a reference to the db object and add the rest of the methods, also it supposed to return the dataElf object itself", "and it does all that!",
+  link = makeTest("dataElf module is supposed to be exporting an object with .link method to initialize the dataElf with a reference to the db object and add the rest of the methods, also it supposed to return the dataElf object itself", "and it does all that!",
     async (fail, crit)=> {
-
-    const methods = csv('user, addUser, updUser',
-      'ses, addSes, updSes, delSes, sess, siftSess'), absent = []
 
     if (typeof dataElf != 'object')
       fail("dataElf's index.js doesn't export an object")
@@ -34,22 +31,54 @@ const
     if (!dataElf.link || typeof dataElf.link != 'function')
       crit("dataElf doesn't have a .link(dbStr) method")
 
-    const linkReturn = await dataElf.link(mongoConnect, {db: 'testdb'})
+    const db =
+      (await MongoClient(mongoConnect, mongoOptions).connect()).db(dbName)
+
+    await db.collection('users').drop()
+
+    const linkReturn = await dataElf.link(mongoConnect, {db: dbName})
 
     if (linkReturn != dataElf)
       fail(".link(dbStr) didn't return the same dataElf object")
 
     if (!dataElf.db || typeof dataElf.db != 'object')
-      fail("dataElf didn't aquire a reference to the db object after .link call")
+      fail(".link didn't add a db reference to the dataElf object")
 
     methods.splice(0, methods.length,...methods.filter(m =>
       dataElf[m] && typeof dataElf[m] == 'function'? 1 : absent.push(m) && 0))
 
     if (absent.length)
-      fail("dataElf supposed to have methods: "+absent.join(', '))
+      fail(".link didn't add methods: "+absent.join(', '))
+
+    if (!dataElf.db.users || dataElf.db.users.namespace!="testdb.users")
+      fail(".link didn't add the db.users collection shorthand")
   })
 
+  addUser = makeTest("dataElf.addUser(login, hash) is supposed to add a user record to the users collection in the db and should return that record's id", "and it does.", async (fail, crit)=> {
+
+    if (!methods.includes('addUser')) crit('method not found')
+
+    const bob = {login: 'Bob', hash: 'a098d7f0a8s7df'},
+          timestamp = Date.now(),
+
+          id = await dataElf.addUser(...values(bob))
+
+    if (!id || typeof id != 'string')
+      crit("dataElf.addUser(login, hash) didn't return the record id")
+
+    const accepted = await dataElf.addUser(...values(bob))
+
+    if (accepted) fail("dataElf.addUser(login, hash) didn't return false when login occupied")
+
+    const record =
+      await dataElf.db.collection('users').findOne({_id: ObjectId(id)})
+
+    if (record._id.toString()!=id || record.login!=bob.login ||
+        record.hash!=bob.hash || record.created-timestamp<0 ||
+        record.created-timestamp>3e4)
+      fail("dataElf.addUser(login, hash) didn't add the record correctly")
+  })
 
 runTests()
 
-assign(global, {dataElf, c})
+assign(global, {dataElf, c, ObjectId})
--- index.js
+++ index.js
@@ -3,13 +3,38 @@ let db
 
 const
   { assign, entries } = Object,
-  { MongoClient, ObjectId, Timestamp } = require('mongodb'),
+  { MongoClient, ObjectId } = require('mongodb'),
   c = console.log,
 
+  prepUsers = async ()=> {
+    await db.createCollection("users", {validator: {$jsonSchema: {
+      bsonType: "object",
+      required: ["login", "hash"],
+      properties: {
+        login: {bsonType: "string", minLength: 2, maxLength: 32},
+        hash: {bsonType: "string", minLength: 10}
+      }
+    }}})
+    db.users = db.collection('users')
+    await db.users.createIndex({login: 1}, {unique: true})
+
+  },
+
+  addUser = async (login, hash)=> {
+    try {
+      return (await db.users.insertOne({login, hash, created: Date.now()}))
+        .insertedId.toString()
+    } catch (err) {
+      if ([121, 11000].includes(err.code)) return false
+      else throw err
+    }
+  },
+
   link = async (str, options={})=> {
-    db = (await new MongoClient(str, {useUnifiedTopology: true}).connect())
+    db = (await MongoClient(str, {useUnifiedTopology: true}).connect())
       .db(options.db || 'lldb')
-    return assign(exports, {db})
+    await prepUsers()
+    return assign(exports, {db, addUser})
   }
 
 
